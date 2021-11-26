4c70810d1326fcddce3df09f8e69b9e352445321
jasmo <jasmo@umich.edu>
2021-11-26 17:04:52 -0500

    Added plant progress bars

--- scripts/page.js
+++ scripts/page.js
@@ -11,6 +11,15 @@ var appView = new Vue({
     new_username: "",
     new_password: "",
     new_confirm_password: "",
+    plant_1_progress_value: 0,
+    plant_1_add_progress_value: 0,
+    plant_1_level: 1,
+    plant_2_progress_value: 0,
+    plant_2_add_progress_value: 0,
+    plant_2_level: 1,
+    plant_3_progress_value: 0,
+    plant_3_add_progress_value: 0,
+    plant_3_level: 1,
   },
   methods: {
     sendLogin:function() {
@@ -46,6 +55,33 @@ var appView = new Vue({
     redirectMainScreen: function() {
       this.activity_selection_class = "hidden";
       this.main_screen_class = "";
-    }
+    },
+    addProgressPlant1: function() {
+      this.plant_1_progress_value += parseInt(this.plant_1_add_progress_value) * 10;
+      if (this.plant_1_progress_value >= 100) {
+        this.plant_1_progress_value = this.plant_1_progress_value % 100;
+        this.plant_1_level++;
+      }
+      console.log(this.plant_1_progress_value);
+      console.log("Plant 1: level " + this.plant_1_level);
+    },
+    addProgressPlant2: function() {
+      this.plant_2_progress_value += parseInt(this.plant_2_add_progress_value) * 10;
+      if (this.plant_2_progress_value >= 100) {
+        this.plant_2_progress_value = this.plant_2_progress_value % 100;
+        this.plant_2_level++;
+      }
+      console.log(this.plant_2_progress_value);
+      console.log("Plant 2: level " + this.plant_2_level);
+    },
+    addProgressPlant3: function() {
+      this.plant_3_progress_value += parseInt(this.plant_3_add_progress_value) * 10;
+      if (this.plant_3_progress_value >= 100) {
+        this.plant_3_progress_value = this.plant_3_progress_value % 100;
+        this.plant_3_level++;
+      }
+      console.log(this.plant_3_progress_value);
+      console.log("Plant 3: level " + this.plant_3_level);
+    },
   }
 })
