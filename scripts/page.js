1315211d0329c07e05a99df40af620a13aa7c7c7
jordanfurr <ejfurr@umich.edu>
2021-11-27 15:02:44 -0500

    cors override

--- scripts/page.js
+++ scripts/page.js
@@ -1,3 +1,18 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js'
+
+  const firebaseConfig = {
+  apiKey: "AIzaSyCRkfhPULJP3gdkPvfCs96bmnNpzJr3m4Y",
+  authDomain: "sunlightly-2a739.firebaseapp.com",
+  projectId: "sunlightly-2a739",
+  storageBucket: "sunlightly-2a739.appspot.com",
+  messagingSenderId: "130278659080",
+  appId: "1:130278659080:web:a4f406c6369a2716d62dbd",
+  measurementId: "G-5VKCJRK2RF"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  console.log(firebaseApp)
+            
 var appView = new Vue({
   el: '#app',
   data: {
