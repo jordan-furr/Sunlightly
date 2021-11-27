518a1170b97110273f42d0d65fefc2d0d15efa6f
colyoon <colyoon@umich.edu>
2021-11-27 04:00:50 -0500

    Working frontend, still need backend functionality and final assets

--- scripts/page.js
+++ scripts/page.js
@@ -11,15 +11,31 @@ var appView = new Vue({
     new_username: "",
     new_password: "",
     new_confirm_password: "",
-    plant_1_progress_value: 0,
-    plant_1_add_progress_value: 0,
-    plant_1_level: 1,
-    plant_2_progress_value: 0,
-    plant_2_add_progress_value: 0,
-    plant_2_level: 1,
-    plant_3_progress_value: 0,
-    plant_3_add_progress_value: 0,
-    plant_3_level: 1,
+    added_sunlight: 0,
+    activities: [
+      ["Sports", ["Play Basketball", "Play Soccer", "Play Tennis"]],
+      ["Hiking", ["Visit Birds Hill Nature Area", "Walk the Ann Arbor Ramble Trail", "Hike the Saginaw Forest Loop"]],
+      ["Touring", ["Visit the University of Michigan Museum of Art", "Eat at Zingerman's Delicatessen", "Visit the Ann Arbor Hands-On Museum"]],
+      ["Wellness", ["Meditate Outside", "Hang Out with a Friend Outside", "Go Birdwatching"]]
+    ],
+    plants: [
+      ["Rose", ["images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png"]],
+      ["Daffodil", ["images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png"]],
+      ["Daisy", ["images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png"]],
+      ["Dandelion", ["images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png", "images/flower.png"]]
+    ],
+    temp_plant: {
+      name: "",
+      images: [],
+      growth: 0,
+      activity: "",
+      bonuses: [],
+    },
+    active_plants: [
+      {name: "Empty", images: [], growth: 0, activity: "", bonuses: [], added_value: 0, current_bonus: ""},
+      {name: "Empty", images: [], growth: 0, activity: "", bonuses: [], added_value: 0, current_bonus: ""},
+      {name: "Empty", images: [], growth: 0, activity: "", bonuses: [], added_value: 0, current_bonus: ""}
+    ]
   },
   methods: {
     sendLogin:function() {
@@ -43,45 +59,73 @@ var appView = new Vue({
     logOut: function() {
       this.login_class = "";
       this.main_screen_class = "hidden";
+      this.active_plants = [
+        {name: "Empty", images: [], growth: 0, activity: "", bonuses: [], added_value: 0, current_bonus: ""},
+        {name: "Empty", images: [], growth: 0, activity: "", bonuses: [], added_value: 0, current_bonus: ""},
+        {name: "Empty", images: [], growth: 0, activity: "", bonuses: [], added_value: 0, current_bonus: ""}
+      ];
     },
     addPlant: function() {
+      this.temp_plant = {};
       this.main_screen_class = "hidden";
       this.plant_selection_class = "";
     },
-    selectActivity: function() {
+    removePlant: function(plant_index) {
+      Vue.set(this.active_plants, plant_index, {name: "Empty", images: [], growth: 0, activity: "", bonuses: [], added_value: 0, current_bonus: ""})
+    },
+    selectActivity: function(plant_index) {
+      this.temp_plant.name = this.plants[plant_index][0];
+      this.temp_plant.images = this.plants[plant_index][1];
+      this.temp_plant.growth = 0;
       this.plant_selection_class = "hidden";
       this.activity_selection_class = "";
     },
-    redirectMainScreen: function() {
+    redirectMainScreen: function(activity_index) {
+      this.temp_plant.activity = this.activities[activity_index][0];
+      this.temp_plant.bonuses = this.activities[activity_index][1];
+      this.temp_plant.current_bonus = this.temp_plant.bonuses[Math.floor(Math.random()*this.temp_plant.bonuses.length)];
+      this.added_value = 0;
+      for (let i = 0; i < 3; i++) {
+        if (this.active_plants[i].name === "Empty") {
+          Vue.set(this.active_plants, i, this.temp_plant);
+          break;
+        }
+      }
       this.activity_selection_class = "hidden";
       this.main_screen_class = "";
     },
-    addProgressPlant1: function() {
-      this.plant_1_progress_value += parseInt(this.plant_1_add_progress_value) * 10;
-      if (this.plant_1_progress_value >= 100) {
-        this.plant_1_progress_value = this.plant_1_progress_value % 100;
-        this.plant_1_level++;
+    addSunlight: function() {
+      if (!isNaN(this.added_sunlight))
+      {
+        for (let i = 0; i < 3; i++) {
+          if (this.active_plants[i].name != "Empty") {
+            let plant_copy = Object.assign({}, this.active_plants[i]);
+            plant_copy.growth = Math.min(parseInt(plant_copy.growth) + (parseInt(this.added_sunlight) * 5), 100);
+            Vue.set(this.active_plants, i, plant_copy);
+          }
+        }
       }
-      console.log(this.plant_1_progress_value);
-      console.log("Plant 1: level " + this.plant_1_level);
     },
-    addProgressPlant2: function() {
-      this.plant_2_progress_value += parseInt(this.plant_2_add_progress_value) * 10;
-      if (this.plant_2_progress_value >= 100) {
-        this.plant_2_progress_value = this.plant_2_progress_value % 100;
-        this.plant_2_level++;
+    addWater: function(plant_index) {
+      if (!isNaN(this.active_plants[plant_index].added_value)) {
+        let plant_copy = Object.assign({}, this.active_plants[plant_index]);
+        plant_copy.growth = Math.min(parseInt(plant_copy.growth) + parseInt(plant_copy.added_value) * 10, 100);
+        Vue.set(this.active_plants, plant_index, plant_copy);
       }
-      console.log(this.plant_2_progress_value);
-      console.log("Plant 2: level " + this.plant_2_level);
     },
-    addProgressPlant3: function() {
-      this.plant_3_progress_value += parseInt(this.plant_3_add_progress_value) * 10;
-      if (this.plant_3_progress_value >= 100) {
-        this.plant_3_progress_value = this.plant_3_progress_value % 100;
-        this.plant_3_level++;
+    addBonus: function(plant_index) {
+      new_bonus = this.active_plants[plant_index].bonuses[Math.floor(Math.random()*this.active_plants[plant_index].bonuses.length)];
+      let plant_copy = Object.assign({}, this.active_plants[plant_index]);
+      plant_copy.current_bonus = new_bonus;
+      plant_copy.growth = Math.min(parseInt(plant_copy.growth) + 30, 100);
+      Vue.set(this.active_plants, plant_index, plant_copy);
+    },
+    bindImage: function(plant_index) {
+      if (this.active_plants[plant_index].name != "Empty") {
+        return this.active_plants[plant_index].images[Math.floor(4 * (this.active_plants[plant_index].growth / 100.0))];
+      } else {
+        return "";
       }
-      console.log(this.plant_3_progress_value);
-      console.log("Plant 3: level " + this.plant_3_level);
     },
   }
 })
